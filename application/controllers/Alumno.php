ffdf7ff50e54fe3fa997e9b81ced452fa0194b94
UTF2390 <UTF2390@192.168.123.157>
2017-05-09 14:52:07 +0200

    Cambio gordo en la bbdd relacionado con presentacion.

--- application/controllers/Alumno.php
+++ application/controllers/Alumno.php
@@ -5,43 +5,53 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Alumno extends CI_Controller {
 
     public function index() {
-        $this->load->view('vista_alumno');
+        $this->load->view('vista_alumno'); 
     }
 
     public function apuntarse($id_tarea) {
         //1.-Comprobar si la tarea se permite por curso del alumno.
+        //
         //2.-Comprobar si la el alumno ha superado el limite por categoria
         //permitidos en los ultimos 30 dias en caso de que la opcion de 
         //libre_de_limites este en FALSE.
+        //
         //3.-Comprobar si el taller tiene plazas libres.
+        //
         //4.-El alumno solo puede apuntarse a talleres con fecha de semana 
         //siguiente o actual nunca del pasado.
+        //
         //5.-Comprobar si ya esta apuntado a esta tarea. Comparar la ultima
         //fecha de la tabla apuntar, si es posterior a la fecha actual el 
         //alumno ya esta apuntado.
+        //
         //6.-Si los puntos anteriores lo permite apuntar al alumno a la tarea.
+        //
+        //7.-Ir al metodo presentaciones.
     }
 
     public function desapuntarse() {
         //1.-Comprobar si el alumno esta apuntado a la tarea (si la fecha de
         //la tarea es posterior al dia actual, esta apuntado). 
-        
+        //
         //2.-Borrar la ultima fecha que sea mayor a fecha actual.
-        
-        //3.-Volver a vista_presentaciones
+        //
+        //3.-Volver a vista_presentaciones.
     }
 
+    //Muestra una lista de los talleres y presentaciones a las que el alumno
+    //se apuntó.
     public function historial() {
         //Pasar por parametro el array del historial a la vista y mostrar
         //vist_historial.
     }
-    
-    public function presentaciones() {
-        //1.-Conseguir presentaciones permitidas en el curso del alumno incluye 
-        //las que ya esta apuntado.
 
-        //2.-Mostrar vista_presentaciones. Le pasamos por parametro el array
-        //que tiene que mostrar (Todos)
+    //Muestra las presentaciónes a las que el alumno se puede apuntar.
+    public function presentacion() {
+        //1.-Conseguir tareas que pertenezcan a la presentacion del alumno curso.
+        //
+        //
+        //2.-Mostrar vista_tareas. Le pasamos por parametro el array
+        //que tiene que mostrar.
     }
 
 }
--- application/controllers/Profesor.php
+++ application/controllers/Profesor.php
@@ -8,4 +8,17 @@ class Profesor extends CI_Controller {
         $this->load->view('vista_profesor');
     }
 
+    public function crear_presentacion() {
+
+        }
+
+    public function nueva_tarea() {
+        //1.-Recoger los datos por post.
+        //
+        //2.-Comprobar si existe la presentación.
+        //
+        //3.-Comprobar si el profesor tiene otras tareas a la misma hora.
+        //
+        }
+
 }
